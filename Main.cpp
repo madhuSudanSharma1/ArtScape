0668950a21d7a2f1aeb263547ca04b54d5a688aa
Madhu Sharma <077bct042.madhu@pcampus.edu.np>
2023-07-31 16:54:16 +0545

    pillar img coord changen

--- Main.cpp
+++ Main.cpp
@@ -187,7 +187,6 @@ int main()
            -50.5f, 21.5f, 75.5f, 1.0f, 0.0f,
     };
 
-
     float verticesTopBottom[] = {
         50.5f, 21.5f, -75.5f, 1.0f, 1.0f,
         -50.5f, 21.5f, -75.5f, 0.0f, 1.0f,
@@ -203,7 +202,6 @@ int main()
         -50.5f, -21.5f, 75.5f, 0.0f, 0.0f,
         50.5f, -21.5f, 75.5f, 1.0f, 0.0f
     };
-
 
     float verticesPillarBack[] = {
         //front
@@ -237,7 +235,7 @@ int main()
 
     };
 
-    float verticesPillarImage[] = {
+    /*float verticesPillarImage[] = {
         //front
        -16.5f, 17.5f, 20.5f, 0.0f, 1.0f,
        -16.5f, -17.5f, 20.5f, 0.0f, 0.0f,
@@ -246,6 +244,7 @@ int main()
        16.5f, -17.5f, 20.5f, 1.0f, 0.0f,
        16.5f, 17.5f, 20.5f, 1.0f, 1.0f,
 
+       //Back
        -16.5f, 17.5f, -20.5f, 0.0f, 1.0f,
        -16.5f, -17.5f, -20.5f, 0.0f, 0.0f,
        16.5f, -17.5f, -20.5f, 1.0f, 0.0f,
@@ -253,22 +252,30 @@ int main()
        16.5f, -17.5f, -20.5f, 1.0f, 0.0f,
        16.5f, 17.5f, -20.5f, 1.0f, 1.0f,
 
-       16.5f, -17.5f, 20.5f, 0.0f, 1.0f,
-       16.5f, -17.5f, -20.5f, 0.0f, 0.0f,
-       16.5f, 17.5f, -20.5f, 1.0f, 0.0f,
-       16.5f, -17.5f, 20.5f, 0.0f, 1.0f,
-       16.5f, 17.5f, -20.5f, 1.0f, 0.0f,
-       16.5f, 17.5f, 20.5f, 1.0f, 1.0f,
+       20.5f, -17.5f, 16.5f, 0.0f, 1.0f,
+       20.5f, -17.5f, -16.5f, 0.0f, 0.0f,
+       20.5f, 17.5f, -16.5f, 1.0f, 0.0f,
+       20.5f, -17.5f, 16.5f, 0.0f, 1.0f,
+       20.5f, 17.5f, -16.5f, 1.0f, 0.0f,
+       20.5f, 17.5f, 16.5f, 1.0f, 1.0f,
 
-       -16.5f, -17.5f, 20.5f, 0.0f, 1.0f,
-       -16.5f, -17.5f, -20.5f, 0.0f, 0.0f,
-       -16.5f, 17.5f, -20.5f, 1.0f, 0.0f,
-       -16.5f, -17.5f, 20.5f, 0.0f, 1.0f,
-       -16.5f, 17.5f, -20.5f, 1.0f, 0.0f,
-       -16.5f, 17.5f, 20.5f, 1.0f, 1.0f,
+       -20.5f, -17.5f, 16.5f, 0.0f, 1.0f,
+       -20.5f, -17.5f, -16.5f, 0.0f, 0.0f,
+       -20.5f, 17.5f, -16.5f, 1.0f, 0.0f,
+       -20.5f, -17.5f, 16.5f, 0.0f, 1.0f,
+       -20.5f, 17.5f, -16.5f, 1.0f, 0.0f,
+       -20.5f, 17.5f, 16.5f, 1.0f, 1.0f,
     };
-
+    */
     
+    float verticesPillarImage[] = {
+        -16.5f, 17.5f, 20.5f, 0.0f, 1.0f,
+       -16.5f, -17.5f, 20.5f, 0.0f, 0.0f,
+       16.5f, -17.5f, 20.5f, 1.0f, 0.0f,
+       -16.5f, 17.5f, 20.5f, 0.0f, 1.0f,
+       16.5f, -17.5f, 20.5f, 1.0f, 0.0f,
+       16.5f, 17.5f, 20.5f, 1.0f, 1.0f,
+    };
     
     // build and compile our shader program
     // ------------------------------------
@@ -282,48 +289,8 @@ int main()
     VAOVBO(VAO, VBO, 0, verticesSide, sizeof(verticesSide),true);
     VAOVBO(VAO, VBO, 1, verticesTopBottom, sizeof(verticesTopBottom),true);
     VAOVBO(VAO, VBO, 2, verticesPillarImage, sizeof(verticesPillarImage),true);
+    
     VAOVBO(VAO, VBO, 3, verticesPillarBack, sizeof(verticesPillarBack),false);
-
-    //glBindVertexArray(VAO[0]);
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(verticesSide), verticesSide, GL_STATIC_DRAW);
-
-    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    //glEnableVertexAttribArray(0);
-
-    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    //glEnableVertexAttribArray(1);
-
-
-    /*glBindVertexArray(VAO[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTopBottom), verticesTopBottom, GL_STATIC_DRAW);
-
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);*/
-    
-    //glBindVertexArray(VAO[2]);
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(verticesPillarImage), verticesPillarImage, GL_STATIC_DRAW);
-
-    //
-    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    //glEnableVertexAttribArray(0);
-
-    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    //glEnableVertexAttribArray(1);
-
-   /* glBindVertexArray(VAO[3]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[3]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesPillarBack), verticesPillarBack, GL_STATIC_DRAW);
-
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);*/
 
 
     unsigned int texture1, texture2,texture3;
@@ -354,7 +321,7 @@ int main()
         
         glBindTexture(GL_TEXTURE_2D, texture3);
         glBindVertexArray(VAO[2]);
-        glDrawArrays(GL_TRIANGLES,0,24);
+        glDrawArrays(GL_TRIANGLES,0,6);
         
         glBindTexture(GL_TEXTURE_2D, texture1);
 
