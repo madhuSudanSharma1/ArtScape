ecfc200d14c5200036422ee953aa7e2b9d2f9b71
Madhu Sharma <077bct042.madhu@pcampus.edu.np>
2023-08-20 09:48:05 +0545

    fixed

--- Main.cpp
+++ Main.cpp
@@ -202,7 +202,7 @@ int main()
 
         //Main Images
         glBindTexture(GL_TEXTURE_2D, image1);
-        glBindVertexArray(VAO[5]);
+        glBindVertexArray(VAO[7]);
         glDrawArrays(GL_TRIANGLES,0,6);
         
         glBindTexture(GL_TEXTURE_2D, image2);
@@ -252,6 +252,7 @@ int main()
         glBindTexture(GL_TEXTURE_2D, image13);
         glBindVertexArray(VAO[19]);
         glDrawArrays(GL_TRIANGLES, 0, 6);
+
         ourShader.setInt("checkTex", 1);
 
         //Pillar
